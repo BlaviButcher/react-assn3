24cf370244dd523a6f463ac981bae9b4337bd89e
Riley Douglas <rileydouglas@outlook.com>
2022-05-15 10:01:16 +1200

    Added skeleton landing

---
+++ src/components/landing.tsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+class Landing extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Projects</h1>
+        <button>Create New Project</button>
+        <div>Project table</div>
+      </div>
+    );
+  }
+}
+
+export default Landing;
